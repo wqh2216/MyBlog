f11472531b10a1b7c9c711544d525600398bc514
wqh2216 <wqh2216@163.com>
2019-12-17 20:51:04 +0800

    change the base, title, repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  base: '/blog-demo/',
-  title: 'blog-demo',
+  base: '/MyBlog/',
+  title: 'MyBlog',
   description: 'Vuepress blog demo',
   head: [
     ['link', { rel: 'icon', href: '/vue-logo.png' }]
   ],
   themeConfig: {
   	// 你的GitHub仓库
-    repo: 'https://github.com/olewaHHH/blog-demo',
+    repo: 'https://github.com/wqh2216/MyBlog',
     // 自定义仓库链接文字。
     repoLabel: 'My GitHub',
   	nav: [
